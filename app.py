d72ada49752a6b1f86f29f896c01242436282745
José Gabriel <112900256+Olotum@users.noreply.github.com>
2024-01-22 16:43:32 -0300

    Add files via upload
    Melhorias e pasta static

--- app.py
+++ app.py
@@ -14,26 +14,20 @@ app = Flask(__name__)
 #Esta linha é um decorador que associa uma função a um caminho URL específico.
 # Neste caso, ele conecta a função hello_world ao caminho raiz do aplicativo (/).
 #Quando um usuário visita a página inicial do aplicativo (geralmente http://localhost:5000/ por padrão), esta função será executada para lidar com a solicitação.
-def hello_word():
+def home():
 #Esta linha define uma função chamada hello_world.
 #Esta função é responsável por gerar a resposta que será enviada de volta ao navegador da web do usuário.
-    return "<h2>Auça Flask11</h2>"
+    #Renderiza um modelo
+    return render_template("home.html")
 #Nesse exemplo estamos usando o <usuario> para passar dados como parametro pela rota.
 @app.route('/sobre/<usuario>')
 def sobre(usuario):
     return f"<h3>Sobre mim,{usuario}</h3>"
 
-@app.route('/home')
-def curriculo():
-    #Renderiza um modelo
-    return render_template("home.html")
-
 @app.route('/curriculo')
-def home():
+def curriculo():
     return render_template("curriculo.html")
-#Esta linha retorna uma string HTML contendo um cabeçalho "H2"
-#Com o texto "Hello World".Esta string será o conteúdo da página da web
-#Que o usuário vê quando visita o URL raiz do aplicativo.
+    
 if __name__ == '__main__':
     app.run(debug=True)
 #Esta linha testa se o módulo atual é o módulo principal. Se for, executa a função app.run().
